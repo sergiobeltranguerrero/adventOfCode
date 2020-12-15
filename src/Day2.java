09901c49250b4a30b410a11f9e420356e5ada7fb
Sergio Beltrán Guerrer <45850795+sergiobeltranguerrero@users.noreply.github.com>
2020-12-15 16:29:48 +0100

    Add Day2

--- src/Day2.java
+++ src/Day2.java
@@ -1,5 +1,9 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Day2 {
@@ -18,6 +22,51 @@ public class Day2 {
             passwords.add(password);
         }
         this.input = passwords;
-        System.out.println(passwords);
+    }
+
+    public int part0() {
+        Iterator<String> iterator = input.iterator();
+        int              output   = 0;
+        while (iterator.hasNext()) {
+            String[] password        = iterator.next().split(":");
+            String   currentPassword = password[1].strip();
+            String[] policy          = password[0].split(" ");
+            String   character       = policy[1];
+            String[] minAndMMax      = policy[0].split("-");
+            int      minCharacters   = Integer.parseInt(minAndMMax[0]);
+            int      maxCharacters   = Integer.parseInt(minAndMMax[1]);
+            int      counter         = 0;
+            for (int i = 0; i < currentPassword.length(); i += 1) {
+                if (currentPassword.charAt(i) == character.charAt(0)) {
+                    counter += 1;
+                }
+            }
+            if (minCharacters <= counter && counter <= maxCharacters) {
+                output += 1;
+            }
+        }
+        return output;
+    }
+
+    public int part1() {
+        Iterator<String> iterator = input.iterator();
+        int              output   = 0;
+        while (iterator.hasNext()) {
+            String[] password        = iterator.next().split(":");
+            String   currentPassword = password[1].strip();
+            String[] policy          = password[0].split(" ");
+            String   character       = policy[1];
+            String[] minAndMMax      = policy[0].split("-");
+            int      minCharacters   = Integer.parseInt(minAndMMax[0]);
+            int      maxCharacters   = Integer.parseInt(minAndMMax[1]);
+            if (isCorrect(currentPassword, character, minCharacters, maxCharacters)) {
+                output += 1;
+            }
+        }
+        return output;
+    }
+
+    private boolean isCorrect(String currentPassword, String character, int minCharacters, int maxCharacters) {
+        return currentPassword.charAt(minCharacters - 1) == character.charAt(0) && currentPassword.charAt(maxCharacters - 1) != character.charAt(0) || currentPassword.charAt(minCharacters - 1) != character.charAt(0) && currentPassword.charAt(maxCharacters - 1) == character.charAt(0);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException, IllegalAccessException {
         /*DAY 1*/
-       /* Day1 day1 = new Day1("resources/Day1.txt");
+        Day1 day1 = new Day1("resources/Day1.txt");
         System.out.println(day1.part0());
-        System.out.println(day1.part1());*/
+        System.out.println(day1.part1());
 
         /*DAY 2*/
         Day2 day2 = new Day2("resources/Day2.txt");
-
+        System.out.println(day2.part0());
+        System.out.println(day2.part1());
     }
 }
